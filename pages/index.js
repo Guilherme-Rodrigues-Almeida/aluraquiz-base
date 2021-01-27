1cfee7fcae8e63e759c90f9ed6dc1e6bce64815d
Guilherme <guilhermealmeida219@gmail.com>
2021-01-26 22:41:03 -0200

    Código ao final da primeira aula da imersao react

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,56 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.secondary};
 `
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+              <h1>Universo Marvel</h1>
+          </Widget.Header>
+
+          <Widget.Content>
+            <p>Quiz dos filmes da Marvel</p> 
+          </Widget.Content>
+        </Widget>  
+
+        <Widget>
+          <Widget.Content>
+              <h1>Quiz da galera...</h1>
+
+              <p>Quiz dos filmes da Marvel</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/Guilherme-Rodrigues-Almeida" /> 
+    </QuizBackground>
+  );
 }
